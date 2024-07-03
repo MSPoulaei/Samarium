95b7de46af0a9074aba7b315ac3757f363c8f120
sobhankazemi <sobhankazemi80@gmail.com>
2024-07-03 11:36:14 +0330

    [ADD]: getting current user's location functionality

--- app/src/main/java/ir/tehranshomal/samarium/MainActivity.kt
+++ app/src/main/java/ir/tehranshomal/samarium/MainActivity.kt
@@ -1,5 +1,6 @@
 package ir.tehranshomal.samarium
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import ir.tehranshomal.samarium.ui.theme.SamariumTheme
+import kotlin.math.log
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val loc = Location(this)
+        val temp =loc.getLocation()
         super.onCreate(savedInstanceState)
         setContent {
             SamariumTheme {
@@ -22,12 +30,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("android")
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
